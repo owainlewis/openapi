47948e3aa97b09ec826d1687e8d181984183c1b0
Owain Lewis <owain.lewis@oracle.com>
2019-10-22 20:49:10 +0100

    Start work on types and setup packages

--- src/Data/OpenAPI/Types.hs
+++ src/Data/OpenAPI/Types.hs
@@ -1,5 +1,16 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Data.OpenAPI.Types where
 
+import GHC.Generics
+
+import qualified Data.Text as T
+
 data OpenAPI = OpenAPI {
+  openapi :: T.Text
+} deriving (Generic, Show)
+
+data Info = Info {
+  title       :: T.Text,
+  description :: Maybe T.Text
 } deriving (Generic, Show)
